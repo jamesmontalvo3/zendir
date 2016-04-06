51e6ca5b0a8260f8fa45a05c401286f3a6e4333e
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-04-06 10:16:27 -0500

    fix col names...again?

--- scan.py
+++ scan.py
@@ -11,31 +11,31 @@ db = MySQLdb.connect(host=config.database["host"],
 cur = db.cursor()
 
 # top level directory to search (have to escape backslashes, f-u Windows)
-# WAS: root = 'C:\\jamesbatch\\sdrive\\ConsoleLogBackup'
-root = config.directoryToAnalyze
-print "Performing analysis on ", root
+# WAS: rootpath = 'C:\\jamesbatch\\sdrive\\ConsoleLogBackup'
+rootpath = config.directoryToAnalyze
+print "Performing analysis on ", rootpath
 
-for fullpath, dirs, files in os.walk(root):
-	for name in files:
+for fullpath, dirs, files in os.walk(rootpath):
+	for filename in files:
 
-		filepath = join(fullpath, name)
+		filepath = join(fullpath, filename)
 
 		# get the file extension
 		# use os.path.splitext() to split the filename on the last period
 		# returns an array of two items; take the second by doing [1]
 		# returned string will have the period on the front; strip it with [1:]
 		# make it all lowercase
-		ext = os.path.splitext(name)[1][1:].lower()
+		extension = os.path.splitext(filename)[1][1:].lower()
 
-		if ext == "jpg":
-			ext = "jpeg"
+		if extension == "jpg":
+			extension = "jpeg"
 
-		size = getsize( filepath ) # size of this file
+		bytes = getsize( filepath ) # size of this file
 
 		# path to files with top level removed
 		# this will make it easier to translate between S-drive files being
 		# analyzed on a computer other than JSC-MOD-FS3
-		relativepath = fullpath[len(root):]
+		relativepath = fullpath[len(rootpath):]
 
 		stats = os.stat( filepath )
 
@@ -49,12 +49,12 @@ for fullpath, dirs, files in os.walk(root):
 
 		query = """
 			INSERT INTO files
-			(rootpath,relativepath,filename,extension,bytes,sha1,blockhash,created,modified,accessed)
-			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+			(rootpath,relativepath,filename,extension,bytes,sha1,created,modified,accessed)
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 			"""
 
 		cur.execute(query,
-			(name,ext,size,root,relativepath,sha1,created,modified,accessed))
+			(rootpath,relativepath,filename,extension,bytes,sha1,created,modified,accessed))
 
 	print "Complete with directory", fullpath
 
